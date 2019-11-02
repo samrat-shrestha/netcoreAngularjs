e70b216f4b8a21f8abce3dd7d08f65286607cc19
thebluess <samstha007@gmail.com>
2019-11-02 21:10:00 +0545

    Modified App.js

--- crossTask/App/App.js
+++ crossTask/App/App.js
@@ -3,18 +3,18 @@
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
     $routeProvider.when('/EmployeeList', { //Routing for show list of employee
-        templateUrl: '~/App/Views/EmployeeList.html',
+        templateUrl: 'App/Views/EmployeeList.html',
         controller: 'EmployeeController'
     }).when('/AddEmployee', { //Routing for add employee
-        templateUrl: '/App/Views/AddEmployee.html',
+        templateUrl: 'App/Views/AddEmployee.html',
         controller: 'EmployeeController'
     })
         .when('/EditEmployee/:empId', { //Routing for geting single employee details
-            templateUrl: '/App/Views/EditEmployee.html',
+            templateUrl: 'App/Views/EditEmployee.html',
             controller: 'EmployeeController'
         })
         .when('/DeleteEmployee/:empId', { //Routing for delete employee
-            templateUrl: '/App/Views/DeleteEmployee.html',
+            templateUrl: 'App/Views/DeleteEmployee.html',
             controller: 'EmployeeController'
         })
         .otherwise({ //Default Routing
