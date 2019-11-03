7c1ffb81bc7979da4afbda2f7f111525d976819d
thebluess <samstha007@gmail.com>
2019-11-03 17:18:03 +0545

    Changes in path

--- crossTask/wwwroot/Scripts/App.js
+++ crossTask/wwwroot/Scripts/App.js
@@ -3,18 +3,18 @@
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
     $routeProvider.when('/EmployeeList', { //Routing for show list of employee
-        templateUrl: 'Views/EmployeeList.html',
+        templateUrl: '~/Views/EmployeeList.html',
         controller: 'EmployeeController'
     }).when('/AddEmployee', { //Routing for add employee
-        templateUrl: 'Views/AddEmployee.html',
+        templateUrl: '~/Views/AddEmployee.html',
         controller: 'EmployeeController'
     })
         .when('/EditEmployee/:empId', { //Routing for geting single employee details
-            templateUrl: 'Views/EditEmployee.html',
+            templateUrl: '~/Views/EditEmployee.html',
             controller: 'EmployeeController'
         })
         .when('/DeleteEmployee/:empId', { //Routing for delete employee
-            templateUrl: 'Views/DeleteEmployee.html',
+            templateUrl: '~/Views/DeleteEmployee.html',
             controller: 'EmployeeController'
         })
         .otherwise({ //Default Routing
--- crossTask/wwwroot/Scripts/EmployeeController.js
+++ crossTask/wwwroot/Scripts/EmployeeController.js
@@ -1,4 +1,4 @@
-﻿//var app = angular.module('myApp', ['ngRoute']);
+﻿var app = angular.module('myApp', ['ngRoute']);
 
 
 app.controller("EmployeeController", ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
